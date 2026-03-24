382aec27f514a657d516352932aadc82294aee8c
Rakesh <111850784+ponkamrakesh@users.noreply.github.com>
2026-03-25 04:14:56 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def root():
-    return {"message": "Hello from Prompt Alchemist!"}
+async def root():
+    return "The Health Check Is Successfull..!"
