18e92f00645ddfed94ac3b05c6a3a25a8c8cd083
Rakesh <111850784+ponkamrakesh@users.noreply.github.com>
2026-03-25 03:57:23 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
-from mangum import Mangum
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello from Prompt Alchemist!"}
-
-handler = Mangum(app)
